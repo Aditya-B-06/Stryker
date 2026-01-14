c3afe22a112c94a2837e8812c138db82c739e654
Aditya Banerjee <adityabanerjee115@gmail.com>
2026-01-15 00:22:13 +0530

    rendered the first products

--- src/AllProducts.js
+++ src/AllProducts.js
@@ -1,23 +1,44 @@
-import { productImages } from "../utils/productImages";
+import sig226 from "./images/ProductImages/226X-9-Legion.png";
+import flux from "./images/ProductImages/365FLUX-RXSL-LEFT.png"
+
+
 export const products = [
+  {
+    id: 1,
+    name: "SIG 226 Legion",
+    price: 500,
+    qty: 0,
+    image: sig226,
+    desc: "The P226 X-Five Legion is a full-sized, all-metal, 9mm semi-automatic pistol...",
+    country: "Switzerland",
+    video: "PO4TJaxXQ8Y",
+    supplier: "SIG Sauer",
+    type: "light-arm",
+  },
+  {
+    id: 2,
+    name: "SIG 1911XR",
+    price: 300,
+    qty: 0,
+    image:flux,
+    desc: "Optics-ready stainless 1911 featuring a 5 inch barrel, flat-blade trigger...",
+    country: "Switzerland",
+    video: "",
+    supplier: "SIG Sauer",
+    type: "light-arm",
+  },
+  /*{
+    id: 3,
+    name: "365FLUX RXSL",
+    price: 450,
+    qty: 0,
+    image: productImages["365FLUX-RXSL-LEFT.png"],
+    desc: "A rapid-deploy micro-PDW chassis that transforms a P365 pistol...",
+    country: "Switzerland",
+    video: "",
+    supplier: "SIG Sauer",
+    type: "light-arm",
 
-  //Light arms
-  {id:1,name:"SIG 226 Legion",price:500,qty:0,image:productImages["226X-9-Legion.png"],desc:"The P226 X-Five Legion is a full-sized, all-metal, 9mm semi-automatic pistol. It is a crossover firearm designed to be heavy enough for competitive target shooting while remaining functional for professional duty or home defense.",country:"Switzerland",video:"PO4TJaxXQ8Y",supplier:"SIG Sauer",type:"Pistol"},
-
-  {id:2,name:"SIG 1911XR",price:0,qty:0,image:productImages["SIG-1911XR-E-comm-LEFT.png"],desc:"Optics-ready stainless 1911 featuring a 5 inch barrel, flat-blade trigger, and custom G10 grips. A modern, bead-blasted powerhouse equipped with X-RAY3 night sights and an accessory rail.",country:"Switzerland",video:"",supplier:"SIG Sauer",type:"Pistol"},
-
-  {id:3,name:"",price:0,qty:0,image:"",desc:"",country:"",video:"",supplier:"",type:""},
-
-  {id:4,name:"",price:0,qty:0,image:"",desc:"",country:"",video:"",supplier:"",type:""},
-
-  {id:5,name:"",price:0,qty:0,image:"",desc:"",country:"",video:"",supplier:"",type:""},
-
-  {id:6,name:"",price:0,qty:0,image:"",desc:"",country:"",video:"",supplier:"",type:""},
-
-  {id:7,name:"",price:0,qty:0,image:"",desc:"",country:"",video:"",supplier:"",type:""},
-
-  {id:8,name:"",price:0,qty:0,image:"",desc:"",country:"",video:"",supplier:"",type:""},
-
-  {id:9,name:"",price:0,qty:0,image:"",desc:"",country:"",video:"",supplier:"",type:""},
-
-]
+  },
+  */
+];
